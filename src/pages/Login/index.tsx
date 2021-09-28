064ec9952d7c1efab9d6890904552a4ab4570d90
Henrique Gomes <hgs@icomp.ufam.edu.br>
2021-09-28 14:20:40 -0400

    [Feat] Added Login Page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Button, Grid, Typography } from "@material-ui/core";
-
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-
 import TextField from "@material-ui/core/TextField";
-
 import Link from "@material-ui/core/Link";
+
+import { Formik } from "formik";
+import * as yup from "yup";
 
 const useStyles = makeStyles(
   ({ typography: { pxToRem, ...typography }, ...theme }) =>
@@ -27,25 +27,71 @@ const useStyles = makeStyles(
       title: {
         color: "white",
         fontSize: "4rem",
-        fontWeight: typography.fontWeightBold,
+        fontWeight: "bolder",
         textAlign: "left",
       },
       subTitle: {
         color: "white",
         fontSize: "1.6rem",
-        fontWeight: typography.fontWeightBold,
+        fontWeight: "normal",
         textAlign: "left",
       },
-      box: {},
+      linkPrimary: {
+        textAlign: "center",
+        color: "#0575E6",
+        paddingTop: "1.6rem",
+      },
+      linkSecundary: {
+        textAlign: "center",
+        color: "#333333",
+        opacity: "0.7",
+        paddingTop: "0.8rem",
+      },
+      buttonPrimary: {
+        backgroundColor: "#0575E6",
+        color: "white",
+        marginTop: "1.6rem",
+        padding: "0.8rem 0",
+      },
+      buttonSecundary: {
+        backgroundColor: "white",
+        color: "#0575E6",
+        marginTop: "1.6rem",
+        padding: "0.8rem 2rem",
+      },
+      titleForm: {
+        fontSize: "2.4rem",
+        color: "#333333",
+        fontWeight: "bolder",
+        textAlign: "left",
+      },
+      subTitleForm: {
+        fontSize: "1.6rem",
+        color: "#333333",
+        fontWeight: "normal",
+        textAlign: "left",
+      },
     })
 );
 
 const LoginPage: React.FC = () => {
   const classes = useStyles();
+
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Você precisa digitar um email válido")
+      .required("Este compo é obrigatório"),
+    password: yup
+      .string()
+      .max(25, "Sua senha tem mais de 25 caracteres")
+      .required("Este compo é obrigatório"),
+  });
+
   return (
     <Grid container className={classes.login}>
       <Grid item xs={12} lg={7} className={classes.firstColumn}>
-        <div className={classes.box}>
+        <div>
           <Typography className={classes.title}>SCIP</Typography>
           <Typography className={classes.subTitle}>
             Uma plataforma para gerenciar seu projeto
@@ -63,80 +109,85 @@ const LoginPage: React.FC = () => {
         </div>
       </Grid>
       <Grid item xs lg={5} className={classes.secondColumn}>
-        <div style={{ width: "23rem" }}>
-          <Typography
-            style={{
-              fontSize: "2.4rem",
-              color: "#333333",
-              fontWeight: "bolder",
-              textAlign: "left",
-            }}
-          >
-            Olá, de novo!
-          </Typography>
-          <Typography
-            style={{
-              fontSize: "1.6rem",
-              color: "#333333",
-              fontWeight: "normal",
-              textAlign: "left",
-            }}
-          >
-            Bem-vindo de volta
-          </Typography>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              paddingTop: "2.4rem",
-            }}
-          >
-            <TextField
-              id="outlined-basic"
-              label="Email"
-              variant="outlined"
-              placeholder="Digite o seu email"
-              style={{ marginBottom: "1.6rem" }}
-            />
-            <TextField
-              id="outlined-basic"
-              label="Senha"
-              variant="outlined"
-              placeholder="Digite a sua senha"
-            />
-            <Button
-              style={{
-                backgroundColor: "#0575E6",
-                color: "white",
-                marginTop: "1.6rem",
-                padding: "0.8rem 0",
-              }}
-            >
-              Entrar
-            </Button>
-            <Link
-              href="#"
-              style={{
-                textAlign: "center",
-                color: "#333333",
-                opacity: "0.7",
-                paddingTop: "0.8rem",
-              }}
-            >
-              Esqueceu a senha?
-            </Link>
-            <Link
-              href="#"
-              style={{
-                textAlign: "center",
-                color: "#0575E6",
-                paddingTop: "1.6rem",
-              }}
-            >
-              Ainda não tem cadastro?
-            </Link>
-          </div>
-        </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={async (values, actions) => {
+            try {
+              actions.resetForm();
+            } catch (error) {
+              return "";
+            }
+          }}
+        >
+          {(props) => (
+            <form onSubmit={props.handleSubmit} method="POST">
+              <div style={{ width: "23rem" }}>
+                <Typography className={classes.titleForm}>
+                  Olá, de novo!
+                </Typography>
+                <Typography className={classes.subTitleForm}>
+                  Bem-vindo de volta
+                </Typography>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "2.4rem",
+                  }}
+                >
+                  <TextField
+                    value={props.values.email}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    id="email"
+                    name="email"
+                    label="Email"
+                    variant="outlined"
+                    placeholder="Digite o seu email"
+                    style={{ marginBottom: "0rem" }}
+                    type="email"
+                  />
+                  {props.touched.email && props.errors.email && (
+                    <div style={{ color: "red" }}>{props.errors.email}</div>
+                  )}
+                  <TextField
+                    id="password"
+                    name="password"
+                    label="Senha"
+                    value={props.values.password}
+                    onChange={props.handleChange}
+                    onBlur={props.handleBlur}
+                    variant="outlined"
+                    placeholder="Digite a sua senha"
+                    type="password"
+                    style={{ marginTop: "1.6rem" }}
+                  />
+                  {props.touched.password && props.errors.password && (
+                    <div style={{ color: "red" }}>{props.errors.password}</div>
+                  )}
+                  <Button
+                    style={{
+                      backgroundColor: "#0575E6",
+                      color: "white",
+                      marginTop: "1.6rem",
+                      padding: "0.8rem 0",
+                    }}
+                    type="submit"
+                  >
+                    Entrar
+                  </Button>
+                  <Link href="#" className={classes.linkSecundary}>
+                    Esqueceu a senha?
+                  </Link>
+                  <Link href="#" className={classes.linkPrimary}>
+                    Ainda não tem cadastro?
+                  </Link>
+                </div>
+              </div>
+            </form>
+          )}
+        </Formik>
       </Grid>
     </Grid>
   );
