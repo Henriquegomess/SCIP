ba45969fe500dcbe1893bfd4090b3598edfd9c15
Henrique Gomes <hgs@icomp.ufam.edu.br>
2021-10-14 10:55:53 -0400

    [Fix] Removed deprecated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { CssBaseline, IconButton, ThemeProvider } from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
-import { ProviderContext, SnackbarProvider } from "notistack";
-import React, { useRef } from "react";
+import React from "react";
 import Routes from "./components/Routes";
 // import Theme from "./components/Theme";
 // import { IoCProvider } from "./context/IoCContext/IoCContext";
